60de4738c1b2f6866bcc3bc32fe3a40b477a8645
cweijan <cweijan@163.com>
2023-08-17 15:30:59 +0800

    支持补全style language

--- server/src/modes/vue/index.ts
+++ server/src/modes/vue/index.ts
@@ -2,15 +2,10 @@ import { LanguageMode } from '../../embeddedSupport/languageModes';
 import { SnippetManager, ScaffoldSnippetSources } from './snippets';
 import { Range } from 'vscode-css-languageservice';
 import { EnvironmentService } from '../../services/EnvironmentService';
+import { CompletionItemKind } from 'vscode-languageserver-types';
 
 export function getVueMode(env: EnvironmentService, globalSnippetDir?: string): LanguageMode {
   const snippetManager = new SnippetManager(env.getSnippetFolder(), globalSnippetDir);
-  const scaffoldSnippetSources: ScaffoldSnippetSources = {
-    workspace: '💼',
-    user: '🗒️',
-    vetur: '✌'
-  };
-
   return {
     getId() {
       return 'vue';
@@ -30,22 +25,38 @@ export function getVueMode(env: EnvironmentService, globalSnippetDir?: string): 
       const lines = document.getText().slice(0, offset).split('\n');
       const currentLine = lines[position.line];
 
-      const items = snippetManager ? snippetManager.completeSnippets(scaffoldSnippetSources) : [];
+      let items = snippetManager ? snippetManager.completeSnippets(scaffoldSnippetSources) : [];
 
       // If a line starts with `<`, it's probably a starting region tag that can be wholly replaced
       if (currentLine.length > 0 && currentLine.startsWith('<')) {
-        const replacementRange = Range.create(
-          document.positionAt(offset - currentLine.length),
-          document.positionAt(offset)
-        );
-        items.forEach(i => {
-          if (i.insertText) {
-            i.textEdit = {
-              newText: i.insertText,
-              range: replacementRange
-            };
+        // 优化style补全
+        if (currentLine.includes('style')) {
+          if (currentLine.match(/lang="$/)) {
+            items = [
+              { label: 'less', kind: CompletionItemKind.Field },
+              { label: 'scss', kind: CompletionItemKind.Field },
+              { label: 'sass', kind: CompletionItemKind.Field },
+            ]
+          } else {
+            items = [
+              { label: 'lang', kind: CompletionItemKind.Property },
+              { label: 'scoped', kind: CompletionItemKind.Property }
+            ]
           }
-        });
+        } else {
+          const replacementRange = Range.create(
+            document.positionAt(offset - currentLine.length),
+            document.positionAt(offset)
+          );
+          items.forEach(i => {
+            if (i.insertText) {
+              i.textEdit = {
+                newText: i.insertText,
+                range: replacementRange
+              };
+            }
+          });
+        }
       }
 
       return {
@@ -53,7 +64,7 @@ export function getVueMode(env: EnvironmentService, globalSnippetDir?: string): 
         items
       };
     },
-    onDocumentRemoved() {},
-    dispose() {}
+    onDocumentRemoved() { },
+    dispose() { }
   };
 }
